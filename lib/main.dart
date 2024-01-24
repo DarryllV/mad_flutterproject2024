7c176715c76af45e474130b3f77f5e6b8f8aaf3d
DarryllV <dondarryll8@gmail.com>
2024-01-24 08:40:08 +0800

    edited login page
    - added password field
    - renamed username field to email

--- lib/main.dart
+++ lib/main.dart
@@ -36,26 +36,54 @@ class LoginPage extends StatelessWidget {
           alignment: Alignment.center,  
             child: Column(
               children: [
-                TextField(decoration: InputDecoration(
-                  hintText: 'Username',
-                  hintStyle: TextStyle(
-                    fontWeight: FontWeight.w300,
-                    color: Colors.grey[600],
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey[700],
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.green[100],
-                    ),
-                  ),
-                ),
+                Padding(
+                  padding: EdgeInsets.all(18),
 
+                  child: TextField(decoration: InputDecoration(
+                    hintText: 'Email',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      color: Colors.grey[600],
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green[100],
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.all(20),
+                  ),
                 ),
-                
+              ),
+
+              Padding(
+                  padding: EdgeInsets.all(18),
+
+                  child: TextField(decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      color: Colors.grey[600],
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green[100],
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.all(20),
+                  ),
+                ),
+              ),
+
               ElevatedButton(
                 child: Text('Log in (Placeholder until i implement it for real)'),
                 onPressed: () {
